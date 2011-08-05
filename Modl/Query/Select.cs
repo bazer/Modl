51fc9a4a998bb9024a895e198c9f6da11f061873
Bazer <sebastian.oberg@gmail.com>
2011-08-05 16:01:55 +0200

    Added licensing information to all source files and added the gpl license files.

--- Modl/Query/Select.cs
+++ Modl/Query/Select.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿/*
+Copyright 2011 Sebastian Öberg (https://github.com/bazer)
+
+This file is part of Modl.
+
+Modl is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Modl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Modl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Modl.DatabaseProviders;
-using Modl.Linq;
-using System.Linq.Expressions;
-using System.Collections;
-using Modl.Linq.Parsers;
 using System.Data;
+using System.Linq.Expressions;
+using Modl.Linq.Parsers;
 
 namespace Modl.Query
 {
