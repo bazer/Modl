b8d1ba2e31cd61f0144a70c5e8b666a6c152b0ec
Bazer <sebastian.oberg@gmail.com>
2011-08-12 13:09:13 +0200

    Added license information to new files.

--- Modl/DataAccess/DbAccess.cs
+++ Modl/DataAccess/DbAccess.cs
@@ -18,83 +18,79 @@ along with Modl.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Data.SqlClient;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlServerCe;
-using Modl.DatabaseProviders;
 using Modl.Query;
 
 namespace Modl.DataAccess
 {
-    public class DbAccess
-    {
-        static public void ExecuteNonQuery(params IQuery[] queries)
-        {
-            ExecuteNonQuery(new List<IQuery>(queries));
-        }
-
-        static public void ExecuteNonQuery(List<IQuery> queries)
-        {
-            ExecuteNonQuery(Database.GetDbCommands(queries));
-        }
-
-        static public void ExecuteNonQuery(List<IDbCommand> commands)
-        {
-            for (int i = 0; i < commands.Count; i++)
-            {
-                if (commands[i].Connection.State != ConnectionState.Open)
-                    commands[i].Connection.Open();
-
-                commands[i].ExecuteNonQuery();
-
-                if (i + 1 == commands.Count || commands[i].Connection != commands[i + 1].Connection)
-                    commands[i].Connection.Close();
-            }
-        }
-
-        static public T ExecuteScalar<T>(params IQuery[] queries)
-        {
-            return ExecuteScalar<T>(new List<IQuery>(queries));
-        }
-
-        static public T ExecuteScalar<T>(List<IQuery> queries)
-        {
-            return ExecuteScalar<T>(Database.GetDbCommands(queries));
-        }
-
-        static public T ExecuteScalar<T>(List<IDbCommand> commands)
+	public class DbAccess
+	{
+		static public void ExecuteNonQuery(params IQuery[] queries)
 		{
-            T result = default(T);
-            
-            for (int i = 0; i < commands.Count; i++)
-            {
-                if (commands[i].Connection.State != ConnectionState.Open)
-                    commands[i].Connection.Open();
-
-                object o = commands[i].ExecuteScalar();
-
-                if (i+1 == commands.Count || commands[i].Connection != commands[i+1].Connection)
-                    commands[i].Connection.Close();
-
-                if (o != null)
-                    result = (T)Convert.ChangeType(o, typeof(T));
-            }
-
-            return result;
+			ExecuteNonQuery(new List<IQuery>(queries));
 		}
 
-        static public DbDataReader ExecuteReader(IQuery query)
-        {
-            return ExecuteReader(query.ToDbCommand());
-        }
+		static public void ExecuteNonQuery(List<IQuery> queries)
+		{
+			ExecuteNonQuery(Database.GetDbCommands(queries));
+		}
 
-        static public DbDataReader ExecuteReader(IDbCommand command)
-        {
-            command.Connection.Open();
-            
-            return (DbDataReader)command.ExecuteReader(CommandBehavior.CloseConnection);
-        }
-    }
+		static public void ExecuteNonQuery(List<IDbCommand> commands)
+		{
+			for (int i = 0; i < commands.Count; i++)
+			{
+				if (commands[i].Connection.State != ConnectionState.Open)
+					commands[i].Connection.Open();
+
+				commands[i].ExecuteNonQuery();
+
+				if (i + 1 == commands.Count || commands[i].Connection != commands[i + 1].Connection)
+					commands[i].Connection.Close();
+			}
+		}
+
+		static public T ExecuteScalar<T>(params IQuery[] queries)
+		{
+			return ExecuteScalar<T>(new List<IQuery>(queries));
+		}
+
+		static public T ExecuteScalar<T>(List<IQuery> queries)
+		{
+			return ExecuteScalar<T>(Database.GetDbCommands(queries));
+		}
+
+		static public T ExecuteScalar<T>(List<IDbCommand> commands)
+		{
+			T result = default(T);
+			
+			for (int i = 0; i < commands.Count; i++)
+			{
+				if (commands[i].Connection.State != ConnectionState.Open)
+					commands[i].Connection.Open();
+
+				object o = commands[i].ExecuteScalar();
+
+				if (i+1 == commands.Count || commands[i].Connection != commands[i+1].Connection)
+					commands[i].Connection.Close();
+
+				if (o != null)
+					result = (T)Convert.ChangeType(o, typeof(T));
+			}
+
+			return result;
+		}
+
+		static public DbDataReader ExecuteReader(IQuery query)
+		{
+			return ExecuteReader(query.ToDbCommand());
+		}
+
+		static public DbDataReader ExecuteReader(IDbCommand command)
+		{
+			command.Connection.Open();
+			
+			return (DbDataReader)command.ExecuteReader(CommandBehavior.CloseConnection);
+		}
+	}
 }
