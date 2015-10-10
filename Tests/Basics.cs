4e6e8566195cfdbc9a900cf324676fb6fd939c3a
Sebastian Öberg <sebastian.oberg@gmail.com>
2015-10-10 18:40:44 +0200

    Switched to MIT license.

--- Tests/Basics.cs
+++ Tests/Basics.cs
@@ -1,22 +1,4 @@
-﻿/*
-Copyright 2011-2012 Sebastian Öberg (https://github.com/bazer)
-
-This file is part of Modl.
-
-Modl is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Modl is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with Modl.  If not, see <http://www.gnu.org/licenses/>.
-*/
-using ExampleModel;
+﻿using ExampleModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Modl;
 using Modl.Json;
